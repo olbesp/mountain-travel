3f52af6de89a7d81aa932a93816f75672d1600e3
OlBesp <olbesp365@gmail.com>
2017-10-27 12:25:31 +0300

    Add fadeIn/fadeOut animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   // Menu scrolling
+
   $('.menu-item').eq(0).click(function() {
       $('html, body').animate({
           scrollTop: $('#packages').offset().top
@@ -23,4 +24,39 @@ $(document).ready(function(){
         scrollTop: $('#contact').offset().top
     }, 600);
   });
+
+  // Fade in / fade out effects for photos
+
+  $(window).on("load",function() {
+    function fade() {
+      var animation_height = $(window).innerHeight() * 0.4;
+      var ratio = Math.round((1 / animation_height) * 10000) / 10000;
+
+      $('.js-fade').each(function() {
+
+        var objectTop = $(this).offset().top;
+        var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+          if (objectTop < windowBottom) {
+            if (objectTop < windowBottom - animation_height) {
+              $(this).css({
+                transition: 'opacity 0.1s linear',
+                opacity: 1
+              });
+            } else {
+              $(this).css({
+                transition: 'opacity 0.25s linear',
+                opacity: (windowBottom - objectTop) * ratio
+              });
+            }
+          } else {
+            $(this).css( 'opacity', 0 );
+          }
+      });
+    }
+    $('.js-fade').css( 'opacity', 0 );
+    fade();
+    $(window).scroll(function() {fade();});
+  });
+
 });
