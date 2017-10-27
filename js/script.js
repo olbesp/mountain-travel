989113b4dd69ca14ed15f9cb487b8c5af510d900
OlBesp <olbesp365@gmail.com>
2017-10-27 11:26:13 +0300

    Add menu scrolling

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-var wallpaper = document.querySelector(".background");
-wallpaper.classList.add("colored-pic");
+$(document).ready(function(){
+
+  // Menu scrolling
+  $('.menu-item').eq(0).click(function() {
+      $('html, body').animate({
+          scrollTop: $('#packages').offset().top
+      }, 600);
+  });
+  $('.menu-item').eq(1).click(function() {
+      $('html, body').animate({
+          scrollTop: $('#destination').offset().top
+      }, 600);
+  });
+  $('.menu-item').eq(2).click(function() {
+      $('html, body').animate({
+          scrollTop: $('#contact').offset().top
+      }, 600);
+  });
+
+  // Button scrolling
+  $('.btn-contact').click(function() {
+    $('html, body').animate({
+        scrollTop: $('#contact').offset().top
+    }, 600);
+  });
+});
